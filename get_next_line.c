69da6f9b997f8a4ed3357ef330e93ad94f773751
Simon GANON <sganon@e1r2p20.42.fr>
2016-01-05 15:58:23 +0100

    Debut gestion d'erreurs

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sganon <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/04 16:15:51 by sganon            #+#    #+#             */
-/*   Updated: 2016/01/04 16:16:08 by sganon           ###   ########.fr       */
+/*   Updated: 2016/01/05 15:52:05 by sganon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,14 @@ static char		*read_til_n(int const fd, char *ret)
 	int			i;
 	int			j;
 	char		buf[BUFF_SIZE +1];
-	char 		*tmp;
 
 	i = 0;
-	while ((i = read(fd, buf, BUFF_SIZE)))
+	while ((i = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		j = 0;
 		buf[i] = 0;
 		if (ret)
-		{
-			tmp = ft_strjoin(ret, buf);
-			//free(ret);
-			ret = tmp;
-		}
+			ret = ft_strjoin(ret, buf);
 		else
 			ret = ft_strdup(buf);	
 		while (buf[j])
@@ -39,6 +34,8 @@ static char		*read_til_n(int const fd, char *ret)
 			j++;
 		}
 	}
+	if (i == -1)
+		return (NULL);
 	return (ret);
 }
 
@@ -50,23 +47,28 @@ int		get_next_line(int const fd, char **line)
 
 	i = 0;
 	buf = NULL;
-	if (!reste)
-		reste = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
-	if (!(buf = read_til_n(fd, reste)))
-		return (-1);
-	while (buf[i] && buf[i] != '\n')
-		i++;
-	if (i > 0)
-	reste = ft_strsub(buf, (i + 1), ft_strlen(buf));
- 	else if (i == 0 && buf[i] == '\n')
+	if (fd >= 0 && fd != 1 && line)
 	{
-		reste = ft_strsub(buf, 0, ft_strlen(buf));
-		reste++;
+		if (!reste)
+			reste = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
+		if (!(buf = read_til_n(fd, reste)))
+			return (-1);
+		while (buf[i] && buf[i] != '\n')
+			i++;
+		if (i > 0)
+			reste = ft_strsub(buf, (i + 1), ft_strlen(buf));
+		else if (i == 0 && buf[i] == '\n')
+		{
+			reste = ft_strsub(buf, 0, ft_strlen(buf));
+			reste++;
+		}
+		else
+			reste = NULL;
+		*line = ft_strsub(buf, 0, i);
+		if (i > 0 || (buf[i] == '\n' && i == 0))
+			return (1);
+		return (0);
 	}
 	else
-		reste = NULL;
-	*line = ft_strsub(buf, 0, i);
-	if (i > 0 || (buf[i] == '\n' && i == 0))
-		return (1);
-	return (0);
+		return (-1);
 }
